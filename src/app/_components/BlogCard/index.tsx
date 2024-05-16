b4f2cb415eeb2c3ce580ecb21956752ecaaf9037
Joey Laya <joeyazlaya@gmail.com>
2024-05-15 22:25:25 -0700

    update and style BlogCard

--- src/app/_components/BlogCard/index.tsx
+++ src/app/_components/BlogCard/index.tsx
@@ -26,35 +26,52 @@ export const BlogCard: React.FC<Props> = (props) => {
   const { title, authors, media, publishedOn, text, slug, className } = props;
 
   return (
-    <article className={className}>
-      <Image src={media.url} alt={media.alt} width={media.width} height={media.height} />
-      <header>
-        <Link href={`/blog/${slug}`}>
-          <h1>{title}</h1>
-        </Link>
-        <div>
-          {authors.map(({ id, name, photo }) => (
-            <div key={id} className="">
-              {photo && <Image src={photo.url} alt={photo.alt} width="40" height="40" />}
-              <p>{name}</p>
-            </div>
-          ))}
-        </div>
-        <p>
-          <time>{formatDateTime(publishedOn)}</time>
-        </p>
-      </header>
-      <div>
-        <RichText content={text} />
-        <Link href={`/blog/${slug}`}>
-          <p>Read More</p>
-        </Link>        
+    <article className={`
+      w-80 rounded-md bg-slate-100 text-slate-600
+      ${className}
+    `}>
+      <div className="w-full h-44 overflow-hidden rounded-t-md">
+        <Image src={media.url} alt={media.alt} width={media.width} height={media.height} />
       </div>
-      {media.attribution && (
-        <p>
-          <small>{media.attribution}</small>
-        </p>
-      )}      
+      <div className="p-6 flex flex-col gap-4">
+        <header className="flex flex-col gap-4">
+          <Link href={`/blog/${slug}`}>
+            <h2 className="font-bold text-xl hover:text-slate-900 transition-colors">{title}</h2>
+          </Link>
+          <div className="flex flex-col gap-2">
+            <div className='flex flex-wrap gap-4'>
+              {authors.map(({ id, name, photo }) => (
+                <div key={id} className="flex flex-row gap-2 items-center">
+                  {photo &&
+                    <div className="w-8 h-8 rounded-full overflow-hidden relative">
+                      <Image
+                        src={photo.url}
+                        alt={photo.alt}
+                        fill
+                        style={{ objectFit: "cover" }}
+                        sizes="100vw"
+                      />
+                    </div>
+                  }
+                  <p className="font-medium text-sm text-teal-700">{name}</p>
+                </div>
+              ))}
+            </div>
+            <p>
+              <time className='text-xs'>{formatDateTime(publishedOn)}</time>
+            </p>
+          </div>
+        </header>
+        <RichText content={text} className="w-full text-sm" />
+        <div className="flex flex-col gap-2">
+          <Link href={`/blog/${slug}`}>
+            <p className="font-semibold text-md text-teal-500 hover:text-teal-600 transition-colors">Read More</p>
+          </Link>
+          {media.attribution && (
+            <p><small>Photo by {media.attribution}</small></p>
+          )}      
+        </div>
+      </div>
     </article>
   );
 };
